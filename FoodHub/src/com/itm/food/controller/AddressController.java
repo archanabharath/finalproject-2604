ad82b30457725a01be066343f20d14550c780458
archanabharath <33553590+archanabharath@users.noreply.github.com>
2017-11-24 01:29:12 -0600

    #Incorporated addresses listing

--- FoodHub/src/com/itm/food/controller/AddressController.java
+++ FoodHub/src/com/itm/food/controller/AddressController.java
@@ -1,5 +1,8 @@
 package com.itm.food.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
@@ -7,13 +10,17 @@ import com.itm.food.dao.Address;
 import com.itm.food.dao.operation.CustomerOperations;
 import com.itm.food.util.UniqueKeyGen;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextField;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
 
 public class AddressController extends BaseController {
 
@@ -49,7 +56,19 @@ public class AddressController extends BaseController {
 	@FXML
 	private Label lblErrorMsg;
 
+	@FXML
+	private JFXListView<Address> addresslistview;
+
+	@FXML
+	private ScrollPane scrollpane;
+
+	@FXML
+	private AnchorPane addressanchor;
+
 	String pageAddrId = null;
+
+	ObservableList<Address> obAddress = null;
+	ArrayList<Address> addressList = null;
 
 	/*
 	 * This is the method that controls the overall functioning of adding a new
@@ -71,12 +90,16 @@ public class AddressController extends BaseController {
 
 		// Reset the input fields after adding the address
 		resetInputFields();
+		handleAddress();
 
 	}
 
 	@Override
 	void init() {
 		super.init();
+		listAddressesOfCustomers();
+		scrollpane.setVisible(false);
+		renderAddressList();
 
 	}
 
@@ -176,4 +199,136 @@ public class AddressController extends BaseController {
 
 	}
 
+	public void listAddressesOfCustomers() {
+
+		try {
+
+			// obAddress = FXCollections
+			// .observableArrayList(getAllAddresses(BaseController.authenticatedCustomer.getCustomerID()));
+			BaseController.authenticatedCustomer
+					.setAddresses(getAllAddresses(BaseController.authenticatedCustomer.getCustomerID()));
+			// addresslistview.setItems(obAddress);
+			// addresslistview.setStyle("-fx-font: normal bold 14px 'Tahoma'");
+
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+
+	}
+
+	public List<Address> getAllAddresses(String pagesCustId) throws Exception {
+
+		Address getAddress = new Address();
+		getAddress.setCustId(pagesCustId);
+		CustomerOperations getCustAddresses = new CustomerOperations();
+
+		return getCustAddresses.getCustomerAddress(getAddress);
+
+	}
+
+	public void renderAddressList() {
+		addressanchor.getChildren().removeAll(addressanchor.getChildren());
+		addressanchor.setPrefHeight(200.0);
+		scrollpane.setVisible(true);
+		for (int i = 0; i < BaseController.authenticatedCustomer.getAddresses().size(); i++) {
+			renderEachAddress(i);
+		}
+
+	}
+
+	public void renderEachAddress(int addressIndex) {
+
+		double requiredAPaneHeight = addressIndex * 200.0;
+		double currentAPaneHeight = addressanchor.getPrefHeight();
+		if (requiredAPaneHeight <= currentAPaneHeight) {
+			addressanchor.setPrefHeight(currentAPaneHeight + 200.0);
+		}
+
+		// <AnchorPane prefHeight="200.0" prefWidth="1180.0"
+		// style="-fx-border-color: lightgrey;">
+		// Add a inner pane container
+
+		// design the pane for holding every address
+		AnchorPane individualPane = new AnchorPane();
+		individualPane.setLayoutY(addressIndex * 200); // TODO INCREMENT THIS 210.0
+		individualPane.setPrefHeight(200.0);
+		individualPane.setPrefWidth(624.0);
+		individualPane.setStyle("-fx-border-color: teal;");
+
+		// add address information to the individual panes
+		// adding customer name first
+		Label lblCustName = new Label();
+		lblCustName.setLayoutX(200.0);
+		lblCustName.setLayoutY(20.0);
+		lblCustName.prefHeight(30.0);
+		lblCustName.prefWidth(600.0);
+		lblCustName.setText(BaseController.authenticatedCustomer.getAddresses().get(addressIndex).getFname() + " "
+				+ BaseController.authenticatedCustomer.getAddresses().get(addressIndex).getLname());
+		lblCustName.setWrapText(true);
+		lblCustName.setFont(new Font(24.0));
+		AnchorPane.setLeftAnchor(lblCustName, 10.0);
+		AnchorPane.setTopAnchor(lblCustName, 20.0);
+		individualPane.getChildren().add(lblCustName);
+
+		// adding address line 1
+		Label lbladdress1 = new Label();
+		lbladdress1.setLayoutX(200.0);
+		lbladdress1.setLayoutY(20.0);
+		lbladdress1.prefHeight(30.0);
+		lbladdress1.prefWidth(600.0);
+		lbladdress1.setText(BaseController.authenticatedCustomer.getAddresses().get(addressIndex).getAddr1());
+		lbladdress1.setWrapText(true);
+		lbladdress1.setFont(new Font(15.0));
+		AnchorPane.setLeftAnchor(lbladdress1, 20.0);
+		AnchorPane.setTopAnchor(lbladdress1, 70.0);
+		AnchorPane.setRightAnchor(lbladdress1, 400.0);
+		individualPane.getChildren().add(lbladdress1);
+
+		// adding address line 2
+		Label lbladdress2 = new Label();
+		lbladdress2.setLayoutX(200.0);
+		lbladdress2.setLayoutY(20.0);
+		lbladdress2.prefHeight(30.0);
+		lbladdress2.prefWidth(600.0);
+		lbladdress2.setText(BaseController.authenticatedCustomer.getAddresses().get(addressIndex).getAddr2());
+		lbladdress2.setWrapText(true);
+		lbladdress2.setFont(new Font(15.0));
+		AnchorPane.setLeftAnchor(lbladdress2, 20.0);
+		AnchorPane.setTopAnchor(lbladdress2, 90.0);
+		AnchorPane.setRightAnchor(lbladdress2, 400.0);
+		individualPane.getChildren().add(lbladdress2);
+
+		// adding city and zip
+		Label lblcity = new Label();
+		lblcity.setLayoutX(200.0);
+		lblcity.setLayoutY(20.0);
+		lblcity.prefHeight(30.0);
+		lblcity.prefWidth(600.0);
+		lblcity.setText(BaseController.authenticatedCustomer.getAddresses().get(addressIndex).getCity() + ",IL "
+				+ BaseController.authenticatedCustomer.getAddresses().get(addressIndex).getPincode());
+		lblcity.setWrapText(true);
+		lblcity.setFont(new Font(15.0));
+		AnchorPane.setLeftAnchor(lblcity, 20.0);
+		AnchorPane.setTopAnchor(lblcity, 110.0);
+		AnchorPane.setRightAnchor(lblcity, 400.0);
+		individualPane.getChildren().add(lblcity);
+
+		// adding address line 2
+		Label lblcontact = new Label();
+		lblcontact.setLayoutX(200.0);
+		lblcontact.setLayoutY(20.0);
+		lblcontact.prefHeight(30.0);
+		lblcontact.prefWidth(600.0);
+		lblcontact.setText(
+				"Phone: " + BaseController.authenticatedCustomer.getAddresses().get(addressIndex).getAddrphoneNo());
+		lblcontact.setWrapText(true);
+		lblcontact.setFont(new Font(15.0));
+		AnchorPane.setLeftAnchor(lblcontact, 20.0);
+		AnchorPane.setTopAnchor(lblcontact, 130.0);
+		AnchorPane.setRightAnchor(lblcontact, 400.0);
+		individualPane.getChildren().add(lblcontact);
+
+		addressanchor.getChildren().add(individualPane);
+
+	}
 }
